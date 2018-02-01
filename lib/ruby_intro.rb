a88c7a357754c9049a184a1bd789d931f192cc46
Aditi Munshi <munshi.adi@gmail.com>
2018-02-01 00:13:28 -0600

    completed answers

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -4,11 +4,9 @@
 
 def sum(arr)
   sum = 0
-  
   if arr.length != 0
     arr.each{ |a| sum += a}
   end
-  
   return sum
 end
 
@@ -31,28 +29,61 @@ def max_2_sum(arr)
 end
 
 def sum_to_n? (arr, n)
-  arr.combination(2).to_a.each{ |a, b| 
-    if a+b == n
+  arr.combination(2).to_a.each do |a, b| 
+    if a + b == n
       return true
-  }
+    end
+  end
+  return false
 end
 
 # Part 2
 
-def hello(name)
-  # YOUR CODE HERE
+def hello name
+  return "Hello, #{name}"
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  s.match(/^[b-df-hj-np-tv-z]/i) != nil
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  if s =~ /^[0-1]+$/
+    s.to_i(base=10) % 4 == 0
+  else
+    return false
+  end
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  def initialize(isbn, price)
+    if (isbn =="" || price <= 0)
+      raise ArgumentError
+    end
+    @price = price
+    @isbn = isbn
+  end
+  #set
+  def isbn=(isbn)
+    @isbn = isbn
+  end
+  #set
+  def price=(price)
+    @price = price
+  end
+  #get
+  def isbn
+    @isbn
+  end
+  #get
+  def price
+    @price
+  end
+  def price_as_string
+    return format("$%.2f",@price)
+  end
+
 end
+
